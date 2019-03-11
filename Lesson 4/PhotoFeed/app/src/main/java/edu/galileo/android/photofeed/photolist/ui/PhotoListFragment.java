c1bd50ee013ec41a2a86947251fb75e538b9424d
Erwin Gonzalez <erwinggonzalez@galileo.edu>
2019-03-11 15:02:22 -0600

    updated photo feed

---
+++ Lesson 4/PhotoFeed/app/src/main/java/edu/galileo/android/photofeed/photolist/ui/PhotoListFragment.java
@@ -0,0 +1,140 @@
+package edu.galileo.android.photofeed.photolist.ui;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
+
+import java.io.ByteArrayOutputStream;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import edu.galileo.android.photofeed.PhotoFeedApp;
+import edu.galileo.android.photofeed.R;
+import edu.galileo.android.photofeed.entities.Photo;
+import edu.galileo.android.photofeed.photolist.PhotoListPresenter;
+import edu.galileo.android.photofeed.photolist.ui.adapters.OnItemClickListener;
+import edu.galileo.android.photofeed.photolist.ui.adapters.PhotoListAdapter;
+
+public class PhotoListFragment extends Fragment implements PhotoListView, OnItemClickListener {
+    @BindView(R.id.container) RelativeLayout container;
+    @BindView(R.id.recyclerView) RecyclerView recyclerView;
+    @BindView(R.id.progressBar) ProgressBar progressBar;
+    @Inject
+    PhotoListAdapter adapter;
+    @Inject
+    PhotoListPresenter presenter;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupInjection();
+        presenter.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        presenter.unsubscribe();
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    private void setupRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setupInjection() {
+        PhotoFeedApp app = (PhotoFeedApp) getActivity().getApplication();
+        app.getPhotoListComponent(this, this, this).inject(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        ButterKnife.bind(this, view);
+        setupRecyclerView();
+        presenter.subscribe();
+        return view;
+    }
+
+    @Override
+    public void onPhotosError(String error) {
+        Snackbar.make(container, error, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showList() {
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideList() {
+        recyclerView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void addPhoto(Photo photo) {
+        adapter.addPhoto(photo);
+    }
+
+    @Override
+    public void removePhoto(Photo photo) {
+        adapter.removePhoto(photo);
+    }
+
+    @Override
+    public void onPlaceClick(Photo photo) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:" + photo.getLatitutde() +"," + photo.getLongitude()));
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onShareClick(Photo photo, ImageView img) {
+        Bitmap bitmap = ((GlideBitmapDrawable)img.getDrawable()).getBitmap();
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/jpeg");
+
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getActivity().getContentResolver(), bitmap, null, null);
+        Uri imageUri =  Uri.parse(path);
+
+        share.putExtra(Intent.EXTRA_STREAM, imageUri);
+        startActivity(Intent.createChooser(share, getString(R.string.photolist_message_share)));
+    }
+
+    @Override
+    public void onDeleteClick(Photo photo) {
+        presenter.removePhoto(photo);
+    }
+}
